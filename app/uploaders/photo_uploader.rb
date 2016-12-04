17cd6bcf0593c5d6053cac5333e37eef66ee250b
ponentesincausa <ponentesincausa@gmail.com>
2016-12-04 16:21:41 -0600

    adds views to render images, mods to photo sized in photo_uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -28,7 +28,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [200, 300]
+  process :resize_to_fill => [500, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -36,7 +36,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-     process :resize_to_fit => [50, 50]
+     process :resize_to_fit => [200, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
