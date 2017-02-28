c569256da01c9a450315643480036ce35f7ac961
ponentesincausa <ponentesincausa@gmail.com>
2017-02-27 21:47:32 -0600

    agrega campos a fotos

---
+++ db/migrate/20170228034602_add_fields_to_photos.rb
@@ -0,0 +1,7 @@
+class AddFieldsToPhotos < ActiveRecord::Migration[5.0]
+  def change
+    add_column :photos, :published, :boolean, after: :exifdate
+    add_column :photos, :publish_date, :datetime, after: :published
+    add_column :photos, :wow, :integer, after: :publish_date
+  end
+end
