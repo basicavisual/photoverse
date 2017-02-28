be6688febf8886976e306298d087c3c7df9c00ae
ponentesincausa <ponentesincausa@gmail.com>
2017-02-27 21:47:32 -0600

    agrega campos a fotos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228023955) do
+ActiveRecord::Schema.define(version: 20170228034602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 20170228023955) do
     t.string   "exifdate"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.boolean  "published"
+    t.datetime "publish_date"
+    t.integer  "wow"
   end
 
 end
