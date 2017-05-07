d915e9c8d1572a5a7cf661bad42ad3ddfb1a87dd
ponentesincausa <ponentesincausa@gmail.com>
2017-05-07 15:01:18 -0500

    changed scoped vies to true on devise initializer. see notes on issue #3

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :authors, controllers: { registrations: 'registrations' }
-  resources :photos
+  devise_for :authors, :controllers => { registrations: 'registrations' }
+  resources :photos, :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
